c723f9332e30862a94258484d81d64b857a6439b
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2006-04-25 16:04:35 +0000

    Added the beginning of the DBI-based cache module.

---
+++ modules/CGI-Application-NetNewsIface/lib/CGI/Application/NetNewsIface/Cache/DBI.pm
@@ -0,0 +1,90 @@
+package CGI::Application::NetNewsIface::Cache::DBI;
+
+use strict;
+use warnings;
+
+use DBI;
+
+=head1 NAME
+
+CGI::Application::NetNewsIface::Cache::DBI - an internally used class to
+form a fast cache of the NNTP data.
+
+=head1 SYNOPSIS
+
+    use CGI::Application::NetNewsIface::Cache::DBI;
+
+    my $cache = CGI::Application::NetNewsIface::Cache::DBI->new(
+        {
+            'nntp' => $nntp,
+            'dsn' => "dbi:SQLite:dbname=foo.sqlite",
+        },
+    );
+
+=head1 FUNCTIONS
+
+=head2 new({ %params })
+
+Constructs a new cache object. Accepts a single argument - a hash ref with
+named parameters. Required parameters are:
+
+=over 4
+
+=item 'nntp'
+
+A handle to the Net::NNTP object that will be used for querying the NNTP
+server.
+
+=item 'dsn'
+
+The DBI 'dsn' for the DBI initialization.
+
+=back
+
+=cut
+
+sub new
+{
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+
+    $self->_initialize(@_);
+
+    return $self;
+}
+
+sub _initialize
+{
+    my $self = shift;
+    my $args = shift;
+
+    $self->{'nntp'} = $args->{'nntp'};
+
+    return 0;
+}
+
+=head1 AUTHOR
+
+Shlomi Fish, C<< <shlomif@iglu.org.il> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-cgi-application-netnewsiface@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=CGI-Application-NetNewsIface>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006 Shlomi Fish, all rights reserved.
+
+This program is released under the following license: MIT X11.
+
+=cut
+
+1;
+
---
+++ modules/CGI-Application-NetNewsIface/t/02-cache-dbi.t
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 1;
+
+use File::Spec;
+use lib File::Spec->catdir(File::Spec->curdir(), "t", "lib");
+
+use CGI::Application::NetNewsIface::Test::MockNNTP;
+use CGI::Application::NetNewsIface::Test::Data1;
+
+use DBI;
+
+use CGI::Application::NetNewsIface::Cache::DBI;
+
+my $db_file = File::Spec->catfile("t", "data", "testdb.sqlite");
+my $dsn = "dbi:SQLite:dbname=$db_file";
+
+sub create_db
+{
+    unlink($db_file);
+    my $dbh = DBI->connect($dsn, "", "");
+    $dbh->do("CREATE TABLE groups (name varchar(255) PRIMARY KEY, idx INTEGER, last_art INTEGER)");
+    $dbh->do("CREATE TABLE articles (group_idx INTEGER, article_idx INTEGER, msg_id varchar(255), parent INTEGER, subject varchar(255), frm varchar(255), date varchar(255))");
+}
+
+{
+    local $Net::NNTP::groups = Data1::get_groups();
+    {
+        my $nntp = Net::NNTP->new("nntp.shlomifish.org");
+        create_db();
+        my $cache = CGI::Application::NetNewsIface::Cache::DBI->new(
+            {
+                'nntp' => $nntp,
+                'dsn' => $dsn,
+            },
+        );
+        # TEST
+        ok ($cache, "Cache was initialized");
+    }
+}
+1;
+
